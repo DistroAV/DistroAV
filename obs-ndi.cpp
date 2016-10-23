aca2b77475ac12daaca930cb192d7d41e6c8137b
Stéphane Lepin <contact@slepin.fr>
2016-10-23 16:51:06 +0200

    Adios line

--- obs-ndi.cpp
+++ obs-ndi.cpp
@@ -31,7 +31,6 @@ bool obs_module_load(void)
 	NDIlib_find_create_t find_desc = { 0 };
 	find_desc.show_local_sources = true;
 	find_desc.p_groups = NULL;
-
 	ndi_finder = NDIlib_find_create(&find_desc);
 
 	ndi_source_info = create_ndi_source_info();
