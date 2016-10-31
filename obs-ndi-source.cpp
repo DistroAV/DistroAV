0fd3ef5ef1603b4c95beb574e3e3867b8baa749a
Stéphane Lepin <contact@slepin.fr>
2016-10-31 19:37:15 +0100

    Switched to LGPL 2.1

--- obs-ndi-source.cpp
+++ obs-ndi-source.cpp
@@ -1,6 +1,25 @@
+/*
+	obs-ndi (NDI I/O in OBS Studio)
+	Copyright (C) 2016 Stéphane Lepin <stephane.lepin@gmail.com>
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 2.1 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+	Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public
+	License along with this library. If not, see <https://www.gnu.org/licenses/>
+*/
+
 #ifdef _WIN32
 #include <Windows.h>
 #endif
+
 #include <obs-module.h>
 #include <util/platform.h>
 #include <util/threading.h>
