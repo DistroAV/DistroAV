1c9298afd9c226e659c96efd62412aad381e1650
Colin Edwards <colin@recursivepenguin.com>
2017-05-28 20:27:26 -0500

    Add OSX to travis

---
+++ CI/install-dependencies-osx.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Exit if something fails
+set -e
+
+# Echo all commands before executing
+set -v
+
+cd ../
+
+# Install Packages app so we can build a package later
+# http://s.sudre.free.fr/Software/Packages/about.html
+wget --retry-connrefused --waitretry=1 https://s3-us-west-2.amazonaws.com/obs-nightly/Packages.pkg
+sudo installer -pkg ./Packages.pkg -target /
+
+brew update
+brew install qt5
+
+# Fetch and untar prebuilt OBS deps that are compatible with older versions of OSX
+wget --retry-connrefused --waitretry=1 https://s3-us-west-2.amazonaws.com/obs-nightly/osx-deps.tar.gz
+tar -xf ./osx-deps.tar.gz -C /tmp
+
+curl -kLO "https://obs-nightly.s3-us-west-2.amazonaws.com/NewTek%20NDI%20SDK%20(Apple).zip" -f --retry 5
+ls
+unzip "./NewTek%20NDI%20SDK%20(Apple).zip"
+
+git clone --recursive https://github.com/jp9000/obs-studio.git
+cd ./obs-studio
+mkdir build
+cd build
+cmake -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DDepsPath=/tmp/obsdeps  ..
+make -j4
+
+cd ../../obs-ndi
