c099f8b5c92b8457ddbe0da3b39f962e6f410baf
Stéphane <contact@slepin.fr>
2017-05-31 14:44:36 +0200

    OS X: remove prefix before binary name

--- CI/before-deploy-osx.sh
+++ CI/before-deploy-osx.sh
@@ -20,7 +20,7 @@ install_name_tool \
 	-change "$QT_PREFIX/lib/QtWidgets.framework/Versions/5/QtWidgets" @rpath/QtWidgets \
 	-change "$QT_PREFIX/lib/QtGui.framework/Versions/5/QtGui" @rpath/QtGui \
 	-change "$QT_PREFIX/lib/QtCore.framework/Versions/5/QtCore" @rpath/QtCore \
-	../build/libobs-ndi.so
+	../build/obs-ndi.so
 
 # Package app
 echo "Generating .pkg"
