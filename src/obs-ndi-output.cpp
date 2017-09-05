6c6471d4f3506d575245369296032568fd83ac98
Stéphane L <contact@slepin.fr>
2017-09-05 20:59:04 +0200

    Output: proper way to eliminate tearing

--- src/obs-ndi-output.cpp
+++ src/obs-ndi-output.cpp
@@ -262,7 +262,7 @@ void ndi_output_rawvideo(void* data, struct video_data* frame) {
         video_frame.line_stride_in_bytes = frame->linesize[0];
     }
 
-    ndiLib->NDIlib_send_send_video_async_v2(o->ndi_sender, &video_frame);
+    ndiLib->NDIlib_send_send_video_v2(o->ndi_sender, &video_frame);
 }
 
 void ndi_output_rawaudio(void* data, struct audio_data* frame) {
