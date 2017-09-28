5ea8ed1cba039bd49f756565f616807ec3c2e7a8
Stéphane <contact@slepin.fr>
2017-09-28 13:20:23 +0200

    osx installer: fix ndi runtime install script

--- installer/osx-install-ndi-runtime.sh
+++ installer/osx-install-ndi-runtime.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
 cd /tmp
-rm ndiruntime.pkg
+rm -f ndiruntime.pkg
 curl -L -o ndiruntime.pkg http://new.tk/NDIRedistV3Apple
 sudo installer -allowUntrusted -package ./ndiruntime.pkg -target /
