d2061c82eeca3065598123ee689e4f78d7ade0c5
Palakis <contact@slepin.fr>
2018-01-12 11:54:57 +0100

    slobs CI: remove arch folders correctly and fix Qt copy

--- CI/prepare-slobs-package.cmd
+++ CI/prepare-slobs-package.cmd
@@ -2,13 +2,10 @@
 
 xcopy C:\projects\obs-ndi\release\* C:\projects\obs-ndi\release-slobs /E /I
 
-rmdir /S /Q "C:\projects\obs-ndi\release-slobs\obs-plugins\32bit"
 xcopy C:\projects\obs-ndi\release-slobs\obs-plugins\64bit\* C:\projects\obs-ndi\release-slobs\obs-plugins\ /E /I
+rmdir /S /Q "C:\projects\obs-ndi\release-slobs\obs-plugins\32bit"
+rmdir /S /Q "C:\projects\obs-ndi\release-slobs\obs-plugins\64bit"
 
-copy %QTDIR32%\bin\Qt5Core.dll C:\projects\obs-ndi\release-slobs\obs-plugins\32bit\
-copy %QTDIR32%\bin\Qt5Gui.dll C:\projects\obs-ndi\release-slobs\obs-plugins\32bit\
-copy %QTDIR32%\bin\Qt5Widgets.dll C:\projects\obs-ndi\release-slobs\obs-plugins\32bit\
-
-copy %QTDIR64%\bin\Qt5Core.dll C:\projects\obs-ndi\release-slobs\obs-plugins\64bit\
-copy %QTDIR64%\bin\Qt5Gui.dll C:\projects\obs-ndi\release-slobs\obs-plugins\64bit\
-copy %QTDIR64%\bin\Qt5Widgets.dll C:\projects\obs-ndi\release-slobs\obs-plugins\64bit\
+copy %QTDIR64%\bin\Qt5Core.dll C:\projects\obs-ndi\release-slobs\obs-plugins\
+copy %QTDIR64%\bin\Qt5Gui.dll C:\projects\obs-ndi\release-slobs\obs-plugins\
+copy %QTDIR64%\bin\Qt5Widgets.dll C:\projects\obs-ndi\release-slobs\obs-plugins\
