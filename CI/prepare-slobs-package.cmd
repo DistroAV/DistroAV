06953e111cfd327896fadc665113eebd1c064cbe
Palakis <contact@slepin.fr>
2018-01-12 10:31:44 +0100

    CI: fix slobs copy calls

--- CI/prepare-slobs-package.cmd
+++ CI/prepare-slobs-package.cmd
@@ -1,5 +1,11 @@
 @echo off
 
 xcopy C:\projects\obs-ndi\release\* C:\projects\obs-ndi\release-slobs /E /I
-copy %QTDIR32%\bin\Qt5Core.dll %QTDIR32%\bin\Qt5Gui.dll %QTDIR32%\bin\Qt5Widgets.dll C:\projects\obs-ndi\release-slobs\obs-plugins\32bit\
-copy %QTDIR64%\bin\Qt5Core.dll %QTDIR64%\bin\Qt5Gui.dll %QTDIR64%\bin\Qt5Widgets.dll C:\projects\obs-ndi\release-slobs\obs-plugins\64bit\
+
+copy %QTDIR32%\bin\Qt5Core.dll C:\projects\obs-ndi\release-slobs\obs-plugins\32bit\
+copy %QTDIR32%\bin\Qt5Gui.dll C:\projects\obs-ndi\release-slobs\obs-plugins\32bit\
+copy %QTDIR32%\bin\Qt5Widgets.dll C:\projects\obs-ndi\release-slobs\obs-plugins\32bit\
+
+copy %QTDIR64%\bin\Qt5Core.dll C:\projects\obs-ndi\release-slobs\obs-plugins\64bit\
+copy %QTDIR64%\bin\Qt5Gui.dll C:\projects\obs-ndi\release-slobs\obs-plugins\64bit\
+copy %QTDIR64%\bin\Qt5Widgets.dll C:\projects\obs-ndi\release-slobs\obs-plugins\64bit\
