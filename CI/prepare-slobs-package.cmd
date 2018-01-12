ea0b72cab8263cd27baae96859303551ab5b607c
Palakis <contact@slepin.fr>
2018-01-12 11:22:13 +0100

    CI: fix slobs package structure (no 32bit)

--- CI/prepare-slobs-package.cmd
+++ CI/prepare-slobs-package.cmd
@@ -2,6 +2,9 @@
 
 xcopy C:\projects\obs-ndi\release\* C:\projects\obs-ndi\release-slobs /E /I
 
+rmdir /S /Q "C:\projects\obs-ndi\release-slobs\obs-plugins\32bit"
+xcopy C:\projects\obs-ndi\release-slobs\obs-plugins\64bit\* C:\projects\obs-ndi\release-slobs\obs-plugins\ /E /I
+
 copy %QTDIR32%\bin\Qt5Core.dll C:\projects\obs-ndi\release-slobs\obs-plugins\32bit\
 copy %QTDIR32%\bin\Qt5Gui.dll C:\projects\obs-ndi\release-slobs\obs-plugins\32bit\
 copy %QTDIR32%\bin\Qt5Widgets.dll C:\projects\obs-ndi\release-slobs\obs-plugins\32bit\
