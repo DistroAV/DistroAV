b242239f269f13df5e35ee98abe810f3d5612c2b
Stéphane L <contact@slepin.fr>
2018-03-30 14:32:13 +0200

    general: license switch to GPL 2.0

--- src/Config.h
+++ src/Config.h
@@ -1,19 +1,19 @@
 /*
-obs-ndi (NDI I/O in OBS Studio)
-Copyright (C) 2016 Stéphane Lepin <stephane.lepin@gmail.com>
+obs-ndi
+Copyright (C) 2016-2018 Stéphane Lepin <steph  name of author
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
-This library is distributed in the hope that it will be useful,
+This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-Lesser General Public License for more details.
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public
-License along with this library. If not, see <https://www.gnu.org/licenses/>
+You should have received a copy of the GNU General Public License
+along with this program; If not, see <https://www.gnu.org/licenses/>
 */
 
 #ifndef CONFIG_H
