602ece11c16829411d9d88959942065970ce33ec
Stéphane Lepin <stephane.lepin@dtforce.com>
2018-06-28 11:50:45 +0200

    ci(linux): remove unwanted transient from libndi deb

--- CI/create-libndi-deb.sh
+++ CI/create-libndi-deb.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+set -ex
 
 LIBS_PATH="/root/ndisdk/lib/x86_64-linux-gnu"
 
-chmod +x $LIBS_PATH/libndi*
 cp $LIBS_PATH/libndi* /usr/lib/
 ln -sf $(ls /usr/lib/libndi* | sort -r | head -1) /usr/lib/libndi.so
+chmod +x /usr/lib/libndi*
