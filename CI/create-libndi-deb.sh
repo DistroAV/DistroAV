b856299a2cf05ab2f6ab517ae7feb27a36b09db2
Stéphane Lepin <stephane.lepin@dtforce.com>
2018-06-28 12:03:32 +0200

    ci(linux): add missing symlink

--- CI/create-libndi-deb.sh
+++ CI/create-libndi-deb.sh
@@ -2,7 +2,10 @@
 set -ex
 
 LIBS_PATH="/root/ndisdk/lib/x86_64-linux-gnu"
-
 cp $LIBS_PATH/libndi* /usr/lib/
-ln -sf $(ls /usr/lib/libndi* | sort -r | head -1) /usr/lib/libndi.so
+
+LATEST_LIB=$(ls /usr/lib/libndi* | sort -r | head -1)
+
+ln -sf $LATEST_LIB /usr/lib/libndi.so
+ln -sf $LATEST_LIB /usr/lib/libndi.so.3
 chmod +x /usr/lib/libndi*
