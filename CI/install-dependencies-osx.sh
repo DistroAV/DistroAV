534cc14b3ae4020c0001070fbc521079b35bcef5
Stéphane L <contact@slepin.fr>
2019-03-03 14:50:39 +0100

    ci(macos): fix azure pipeline + remove travis macos pipeline

--- CI/install-dependencies-osx.sh
+++ CI/install-dependencies-osx.sh
@@ -13,9 +13,9 @@ wget --retry-connrefused --waitretry=1 https://s3-us-west-2.amazonaws.com/obs-ni
 sudo installer -pkg ./Packages.pkg -target /
 
 brew update
-# OBS Studio + obs-websocket deps
+# OBS Studio + obs-ndi deps
 brew install ffmpeg libav
-brew install ./obs-ndi/CI/macos/qt.rb
+brew install https://gist.githubusercontent.com/DDRBoxman/b3956fab6073335a4bf151db0dcbd4ad/raw/ed1342a8a86793ea8c10d8b4d712a654da121ace/qt.rb
 
 # Build OBS Studio
 git clone --recursive https://github.com/jp9000/obs-studio.git
@@ -25,5 +25,3 @@ mkdir build && cd build
 cmake .. \
     -DCMAKE_PREFIX_PATH=/usr/local/opt/qt/lib/cmake \
 && make -j4
-
-cd ../../obs-ndi
