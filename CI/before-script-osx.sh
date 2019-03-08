cdf33a2ec32a41d70fc8f2d27e35d5e82842bd2d
Stéphane Lepin <s.lepin@green-alternative.fr>
2019-03-08 15:32:20 +0100

    ci(macos): don't call make in before script

--- CI/before-script-osx.sh
+++ CI/before-script-osx.sh
@@ -10,5 +10,4 @@ cmake .. \
   -DLIBOBS_LIB=../../obs-studio/libobs \
   -DOBS_FRONTEND_LIB="$(pwd)/../../obs-studio/build/UI/obs-frontend-api/libobs-frontend-api.dylib" \
   -DCMAKE_INSTALL_PREFIX=/usr \
-&& make -j4
 
