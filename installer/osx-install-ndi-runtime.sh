d524e4dfec2388102cb552bb0447aa63e147af00
Stéphane Lepin <stephane.lepin@gmail.com>
2019-09-16 21:32:28 +0200

    macos installer: download V4 runtime

--- installer/osx-install-ndi-runtime.sh
+++ installer/osx-install-ndi-runtime.sh
@@ -2,5 +2,5 @@
 set -ex
 cd /tmp
 rm -f ndiruntime.pkg
-curl -L -o ndiruntime.pkg http://new.tk/NDIRedistV3Apple
+curl -L -o ndiruntime.pkg http://new.tk/NDIRedistV4Apple
 sudo installer -allowUntrusted -package ./ndiruntime.pkg -target /
