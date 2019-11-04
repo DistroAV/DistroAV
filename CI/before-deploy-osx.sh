3823ee391bac770ae124ecd039829e26ca9152db
Stéphane Lepin <stephane.lepin@gmail.com>
2019-11-04 13:07:13 +0100

    ci(macos): fix lookup path of Qt dependencies for new OBS bundle format

--- CI/before-deploy-osx.sh
+++ CI/before-deploy-osx.sh
@@ -12,9 +12,9 @@ export LATEST_FILENAME="obs-ndi-latest-$LATEST_VERSION.pkg"
 cd ./installer
 
 install_name_tool \
-	-change "/usr/local/opt/qt/lib/QtWidgets.framework/Versions/5/QtWidgets" @rpath/QtWidgets \
-	-change "/usr/local/opt/qt/lib/QtGui.framework/Versions/5/QtGui" @rpath/QtGui \
-	-change "/usr/local/opt/qt/lib/QtCore.framework/Versions/5/QtCore" @rpath/QtCore \
+	-change "/usr/local/opt/qt/lib/QtWidgets.framework/Versions/5/QtWidgets" @executable_path/../Frameworks/QtWidgets.framework/Versions/5/QtWidgets \
+	-change "/usr/local/opt/qt/lib/QtGui.framework/Versions/5/QtGui" @executable_path/../Frameworks/QtWidgets.framework/Versions/5/QtGui \
+	-change "/usr/local/opt/qt/lib/QtCore.framework/Versions/5/QtCore" @executable_path/../Frameworks/QtWidgets.framework/Versions/5/QtCore \
 	../build/obs-ndi.so
 
 # Check if replacement worked
