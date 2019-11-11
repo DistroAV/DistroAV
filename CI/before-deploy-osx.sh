0d7fdda3132eaa21636c231a795b8ddc8c7a7bbe
Stéphane Lepin <stephane.lepin@gmail.com>
2019-11-11 14:56:10 +0100

    ci(macos): fix lib paths in package script

--- CI/before-deploy-osx.sh
+++ CI/before-deploy-osx.sh
@@ -13,8 +13,8 @@ cd ./installer
 
 install_name_tool \
 	-change "/usr/local/opt/qt/lib/QtWidgets.framework/Versions/5/QtWidgets" @executable_path/../Frameworks/QtWidgets.framework/Versions/5/QtWidgets \
-	-change "/usr/local/opt/qt/lib/QtGui.framework/Versions/5/QtGui" @executable_path/../Frameworks/QtWidgets.framework/Versions/5/QtGui \
-	-change "/usr/local/opt/qt/lib/QtCore.framework/Versions/5/QtCore" @executable_path/../Frameworks/QtWidgets.framework/Versions/5/QtCore \
+	-change "/usr/local/opt/qt/lib/QtGui.framework/Versions/5/QtGui" @executable_path/../Frameworks/QtGui.framework/Versions/5/QtGui \
+	-change "/usr/local/opt/qt/lib/QtCore.framework/Versions/5/QtCore" @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore \
 	../build/obs-ndi.so
 
 # Check if replacement worked
