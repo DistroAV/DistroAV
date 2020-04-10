f7236a0824ea3d04b6a5c502eff09f73187a0a33
Stéphane Lepin <stephane.lepin@gmail.com>
2020-04-10 10:48:59 +0200

    ci(ubuntu): fix version name generation

--- CI/package-ubuntu.sh
+++ CI/package-ubuntu.sh
@@ -20,11 +20,11 @@ ls /tmp/ndisdk
 
 cd $BASE_DIR
 
-export GIT_HASH=$(git rev-parse --short HEAD)
-export PKG_VERSION="1-$GIT_HASH-$BRANCH_SHORT_NAME-git"
+GIT_HASH=$(git rev-parse --short HEAD)
+PKG_VERSION="1-$GIT_HASH-$BRANCH_SHORT_NAME-git"
 
-if [[ "$BRANCH_FULL_NAME" =~ "^refs/tags/" ]]; then
-	export PKG_VERSION="$BRANCH_SHORT_NAME"
+if [[ $BRANCH_FULL_NAME =~ ^refs\/tags\/ ]]; then
+	PKG_VERSION="$BRANCH_SHORT_NAME"
 fi
 
 cd ./build
