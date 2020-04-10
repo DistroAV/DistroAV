f2b7c2a7ca4b840de1a02914590d6dd249e2e3c0
Stéphane Lepin <Palakis@users.noreply.github.com>
2020-04-10 20:26:35 +0200

    macOS CI: automated release signing and notarization (#434)

--- CI/package-macos.sh
+++ CI/package-macos.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
@@ -12,31 +12,80 @@ fi
 echo "[obs-ndi] Preparing package build"
 export QT_CELLAR_PREFIX="$(/usr/bin/find /usr/local/Cellar/qt -d 1 | sort -t '.' -k 1,1n -k 2,2n -k 3,3n | tail -n 1)"
 
-export GIT_HASH=$(git rev-parse --short HEAD)
-export GIT_BRANCH_OR_TAG=$(git name-rev --name-only HEAD | awk -F/ '{print $NF}')
+GIT_HASH=$(git rev-parse --short HEAD)
+GIT_BRANCH_OR_TAG=$(git name-rev --name-only HEAD | awk -F/ '{print $NF}')
 
-export VERSION="$GIT_HASH-$GIT_BRANCH_OR_TAG"
-export LATEST_VERSION="$GIT_BRANCH_OR_TAG"
+VERSION="$GIT_HASH-$GIT_BRANCH_OR_TAG"
 
-export FILENAME="obs-ndi-$VERSION.pkg"
+FILENAME_UNSIGNED="obs-ndi-$VERSION-Unsigned.pkg"
+FILENAME="obs-ndi-$VERSION.pkg"
 
 echo "[obs-ndi] Modifying obs-ndi.so"
 install_name_tool \
 	-change /usr/local/opt/qt/lib/QtWidgets.framework/Versions/5/QtWidgets \
-        @executable_path/../Frameworks/QtWidgets.framework/Versions/5/QtWidgets \
+		@executable_path/../Frameworks/QtWidgets.framework/Versions/5/QtWidgets \
 	-change /usr/local/opt/qt/lib/QtGui.framework/Versions/5/QtGui \
-        @executable_path/../Frameworks/QtGui.framework/Versions/5/QtGui \
+		@executable_path/../Frameworks/QtGui.framework/Versions/5/QtGui \
 	-change /usr/local/opt/qt/lib/QtCore.framework/Versions/5/QtCore \
-        @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore \
+		@executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore \
 	./build/obs-ndi.so
 
 # Check if replacement worked
 echo "[obs-ndi] Dependencies for obs-ndi"
 otool -L ./build/obs-ndi.so
 
+if [[ "$RELEASE_MODE" == "True" ]]; then
+	echo "[obs-ndi] Signing plugin binary: obs-ndi.so"
+	codesign --sign "$CODE_SIGNING_IDENTITY" ./build/obs-ndi.so
+else
+	echo "[obs-ndi] Skipped plugin codesigning"
+fi
+
 echo "[obs-ndi] Actual package build"
 packagesbuild ./installer/obs-ndi.pkgproj
 
 echo "[obs-ndi] Renaming obs-ndi.pkg to $FILENAME"
 mkdir release
-mv ./installer/build/obs-ndi.pkg ./release/$FILENAME
+mv ./installer/build/obs-ndi.pkg ./release/$FILENAME_UNSIGNED
+
+if [[ "$RELEASE_MODE" == "True" ]]; then
+	echo "[obs-ndi] Signing installer: $FILENAME"
+	productsign \
+		--sign "$INSTALLER_SIGNING_IDENTITY" \
+		./release/$FILENAME_UNSIGNED \
+		./release/$FILENAME
+	rm ./release/$FILENAME_UNSIGNED
+
+	echo "[obs-ndi] Submitting installer $FILENAME for notarization"
+	zip -r ./release/$FILENAME.zip ./release/$FILENAME
+	UPLOAD_RESULT=$(xcrun altool \
+		--notarize-app \
+		--primary-bundle-id "fr.palakis.obs-ndi" \
+		--username "$AC_USERNAME" \
+		--password "$AC_PASSWORD" \
+		--asc-provider "$AC_PROVIDER_SHORTNAME" \
+		--file "./release/$FILENAME.zip")
+	rm ./release/$FILENAME.zip
+
+	REQUEST_UUID=$(echo $UPLOAD_RESULT | awk -F ' = ' '/RequestUUID/ {print $2}')
+	echo "Request UUID: $REQUEST_UUID"
+
+	echo "[obs-ndi] Wait for notarization result"
+	# Pieces of code borrowed from rednoah/notarized-app
+	while sleep 30 && date; do
+		CHECK_RESULT=$(xcrun altool \
+			--notarization-info "$REQUEST_UUID" \
+			--username "$AC_USERNAME" \
+			--password "$AC_PASSWORD" \
+			--asc-provider "$AC_PROVIDER_SHORTNAME")
+		echo $CHECK_RESULT
+
+		if ! grep -q "Status: in progress" <<< "$CHECK_RESULT"; then
+			echo "[obs-ndi] Staple ticket to installer: $FILENAME"
+			xcrun stapler staple ./release/$FILENAME
+			break
+		fi
+	done
+else
+	echo "[obs-ndi] Skipped installer codesigning and notarization"
+fi
