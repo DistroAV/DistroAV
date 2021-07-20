c7c3d0c25f4bf0b03d6e2f9ee3f9670a40d1a3f5
tt2468 <tt2468@gmail.com>
2021-07-19 17:57:37 -0700

    Base: More changes

--- src/obs-ndi.cpp
+++ src/obs-ndi.cpp
@@ -1,31 +1,20 @@
 #ifdef _WIN32
 #include <Windows.h>
 #endif
-
 #include <sys/stat.h>
-
+#include <QMainWindow>
 #include <obs-module.h>
 #include <obs-frontend-api.h>
 #include <util/platform.h>
-#include <QDir>
-#include <QFileInfo>
-#include <QProcess>
-#include <QLibrary>
-#include <QMainWindow>
-#include <QAction>
-#include <QMessageBox>
-#include <QString>
-#include <QStringList>
 
 #include "obs-ndi.h"
 
 OBS_DECLARE_MODULE()
-OBS_MODULE_AUTHOR("Stephane Lepin (Palakis)")
 OBS_MODULE_USE_DEFAULT_LOCALE("obs-ndi", "en-US")
 
 bool obs_module_load(void)
 {
-	blog(LOG_INFO, "Hello! (Version %s)", OBS_NDI_VERSION);
+	blog(LOG_INFO, "Hello! (Plugin Version %s | Linked LibNDI Version %s)", OBS_NDI_VERSION, LIBNDI_VERSION);
 
 	QMainWindow* main_window = (QMainWindow*)obs_frontend_get_main_window();
 
--- src/obs-ndi.h
+++ src/obs-ndi.h
@@ -1,5 +1,9 @@
 #pragma once
 
-#define OBS_NDI_VERSION "5.0.0"
+#include <util/base.h>
+#include <Processing.NDI.Lib.h>
+
+#include "obs-ndi-macros.generated.h"
 
 #define blog(level, msg, ...) blog(level, "[obs-ndi] " msg, ##__VA_ARGS__)
+#define QT_TO_UTF8(str) str.toUtf8().constData()
