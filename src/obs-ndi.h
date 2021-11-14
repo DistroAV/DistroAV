f1da1970396d126a83122943eb3f84acff966b41
tt2468 <tt2468@gmail.com>
2021-11-14 02:53:35 -0800

    Base: Move some defs to macros file

--- src/obs-ndi.h
+++ src/obs-ndi.h
@@ -1,12 +1,8 @@
 #pragma once
 
-#include <util/base.h>
 #include <Processing.NDI.Lib.h>
 
 #include "obs-ndi-macros.generated.h"
-
-#define blog(level, msg, ...) blog(level, "[obs-ndi] " msg, ##__VA_ARGS__)
-#define QT_TO_UTF8(str) str.toUtf8().constData()
 
 // For translations
 #define T_(text) obs_module_text(text)
