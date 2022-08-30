d8c1133d62c73a2d10b790e5abcdce5557339626
night.vs.knight@gmail.com <night.vs.knight@gmail.com>
2022-08-30 04:06:34 -0700

    Fixing clang-format errors

--- src/input.cpp
+++ src/input.cpp
@@ -256,19 +256,19 @@ void ndi_input::ndi_audio_thread()
 void register_ndi_input_info()
 {
 	struct obs_source_info info = {};
-	info.id =				"ndi_input_v5";
-	info.type =				OBS_SOURCE_TYPE_INPUT;
-	info.output_flags =		OBS_SOURCE_ASYNC_VIDEO | OBS_SOURCE_AUDIO | OBS_SOURCE_DO_NOT_DUPLICATE;
-	info.get_name =			[](void *) { return obs_module_text("Input.Name"); };
-	info.create =			[](obs_data_t *settings, obs_source_t *source) -> void * { return new ndi_input(settings, source); };
-	info.destroy =			[](void *data) { delete static_cast<ndi_input *>(data); };
-	info.get_defaults =		ndi_input::defaults;
-	info.get_properties =	[](void *data) -> obs_properties_t * { return static_cast<ndi_input *>(data)->properties(); };
-	info.update =			[](void *data, obs_data_t *settings) { static_cast<ndi_input *>(data)->update(settings); };
-	info.activate =			[](void *data) { static_cast<ndi_input *>(data)->activate(); };
-	info.deactivate =		[](void *data) { static_cast<ndi_input *>(data)->deactivate(); };
-	info.show =				[](void *data) { static_cast<ndi_input *>(data)->show(); };
-	info.hide =				[](void *data) { static_cast<ndi_input *>(data)->hide(); };
+	info.id = "ndi_input_v5";
+	info.type = OBS_SOURCE_TYPE_INPUT;
+	info.output_flags = OBS_SOURCE_ASYNC_VIDEO | OBS_SOURCE_AUDIO | OBS_SOURCE_DO_NOT_DUPLICATE;
+	info.get_name = [](void *) { return obs_module_text("Input.Name"); };
+	info.create = [](obs_data_t *settings, obs_source_t *source) -> void * { return new ndi_input(settings, source); };
+	info.destroy = [](void *data) { delete static_cast<ndi_input *>(data); };
+	info.get_defaults = ndi_input::defaults;
+	info.get_properties = [](void *data) -> obs_properties_t * { return static_cast<ndi_input *>(data)->properties(); };
+	info.update = [](void *data, obs_data_t *settings) { static_cast<ndi_input *>(data)->update(settings); };
+	info.activate = [](void *data) { static_cast<ndi_input *>(data)->activate(); };
+	info.deactivate = [](void *data) { static_cast<ndi_input *>(data)->deactivate(); };
+	info.show = [](void *data) { static_cast<ndi_input *>(data)->show(); };
+	info.hide = [](void *data) { static_cast<ndi_input *>(data)->hide(); };
 
 	obs_register_source(&info);
 }
