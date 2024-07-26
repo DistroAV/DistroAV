92e6160b3fdddc10bd0ccd6784c9d587bf35453b
Paul Peavyhouse <pv@swooby.com>
2024-07-26 12:19:50 -0700

    settings: load/refresh config before showing

--- src/forms/output-settings.cpp
+++ src/forms/output-settings.cpp
@@ -211,6 +211,8 @@ void OutputSettings::showEvent(QShowEvent *)
 {
 	auto conf = Config::Current();
 
+	conf->Load();
+
 	ui->mainOutputGroupBox->setChecked(conf->OutputEnabled);
 	ui->mainOutputName->setText(conf->OutputName);
 	ui->mainOutputGroups->setText(conf->OutputGroups);
