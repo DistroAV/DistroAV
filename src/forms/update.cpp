cadab378e75f4eeefcf8d3950e4e31fe4fccd46e
Felix Kaechele <felix@kaechele.ca>
2024-11-20 11:03:53 -0500

    Fix deprecated Qt API calls
    - QDateTime::setTimeSpec -> QDateTime::setTimeZone
    - QCheckBox::stateChanged -> QCheckBox::checkStateChanged for Qt 6.7 and
      newer
    
    Signed-off-by: Felix Kaechele <felix@kaechele.ca>

--- src/forms/update.cpp
+++ src/forms/update.cpp
@@ -42,6 +42,7 @@ Inspiration(s):
 #include <QPointer>
 #include <QSslSocket>
 #include <QTimer>
+#include <QTimeZone>
 #include <QUrlQuery>
 
 #define UPDATE_TIMEOUT_SEC 10
@@ -169,7 +170,7 @@ public:
 
 		auto utcDateTime = QDateTime::fromString(
 			pluginUpdateInfo.releaseDate, Qt::ISODate);
-		utcDateTime.setTimeSpec(Qt::UTC);
+		utcDateTime.setTimeZone(QTimeZone::utc());
 		auto formattedUtcDateTime =
 			utcDateTime.toString("yyyy-MM-dd hh:mm:ss 'UTC'");
 		textTemp = QString("<h3>%1</h3>")
@@ -182,11 +183,19 @@ public:
 
 		ui->checkBoxAutoCheckForUpdates->setChecked(
 			config->AutoCheckForUpdates());
+#if QT_VERSION < QT_VERSION_CHECK(6, 7, 0)
 		connect(ui->checkBoxAutoCheckForUpdates,
 			&QCheckBox::stateChanged, this, [](int state) {
 				Config::Current(false)->AutoCheckForUpdates(
 					state == Qt::Checked);
 			});
+#else
+		connect(ui->checkBoxAutoCheckForUpdates,
+			&QCheckBox::checkStateChanged, this, [](int state) {
+				Config::Current(false)->AutoCheckForUpdates(
+					state == Qt::Checked);
+			});
+#endif
 
 		connect(ui->buttonSkipThisVersion, &QPushButton::clicked, this,
 			[this, pluginUpdateInfo]() {
