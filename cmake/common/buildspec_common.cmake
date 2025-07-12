323726b8038dd1673a929ddc8a60562c9fecba2e
Trouffman <trouffman@gmail.com>
2025-07-12 10:55:07 -0400

    revert macos cmake extra arg

--- cmake/common/buildspec_common.cmake
+++ cmake/common/buildspec_common.cmake
@@ -59,7 +59,7 @@ function(_setup_obs_studio)
   elseif(OS_MACOS)
     set(_cmake_generator "Xcode")
     set(_cmake_arch "-DCMAKE_OSX_ARCHITECTURES:STRING='arm64;x86_64'")
-    set(_cmake_extra "-DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET} -DCMAKE_ENABLE_SCRIPTING=OFF")
+    set(_cmake_extra "-DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET}")
   endif()
 
   message(STATUS "Configure ${label} (${arch})")
