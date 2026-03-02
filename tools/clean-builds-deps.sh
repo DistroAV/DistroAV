94696530b6fe8b71a9a434421a08d28d6ca73df8
Trouffman <trouffman@gmail.com>
2026-03-02 09:41:09 -0500

    Update build requirement for macos26 + simplify tools scripts (#1419)
    Add deployment variables clean-up for MACOS to allow build on Mcos26 (local usually).
    Rename & rework helper scripts in /tools folder

---
+++ tools/clean-builds-deps.sh
@@ -0,0 +1,7 @@
+echo "Cleaning previous build..."
+rm -r build_macos
+rm -r release
+
+echo "Removing obs-deps..."
+
+echo "Done."
--- tools/sha256-check.sh
+++ tools/sha256-check.sh
@@ -1,5 +1,4 @@
 set -e
 echo
 sha256sum release/RelWithDebInfo/distroav.plugin/Contents/MacOS/distroav
-# sha256sum release/RelWithDebInfo/obs-ndi.plugin/Contents/MacOS/obs-ndi
 echo
