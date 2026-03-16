9038590a72533dd59b5346ef9ebdbf1bd501c59a
Trouffman <trouffman@gmail.com>
2026-03-16 10:26:59 -0400

    update macos script to contain shebang

--- tools/run-obs-debug-macos.sh
+++ tools/run-obs-debug-macos.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 set -e
 # /Applications/OBS.app/Contents/MacOS/OBS --verbose --distroav-verbose --distroav-debug --distroav-update-local --distroav-update-force --distroav-update-last-check-ignore
 /Applications/OBS.app/Contents/MacOS/OBS --distroav-verbose --distroav-debug --distroav-update-local --distroav-update-force --distroav-update-last-check-ignore
